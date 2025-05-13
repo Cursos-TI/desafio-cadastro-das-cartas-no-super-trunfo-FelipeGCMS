c85e28c9b9648a7d2dd0864416d69fdb1ccf7431
FelipeGCMS <felipegustavo.miranda@gmail.com>
2025-05-13 00:47:23 +0000

    feito

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,10 +11,16 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
     char estado1, cod1[2], cidade1[50], estado2, cod2[2], cidade2[50];
-    int populacao1, turisticos1, populacao2, turisticos2;
+    int turisticos1, turisticos2;
+    int result_pop1, result_area1, result_PIB1, result_des_pop1, result_PIB_percap1,
+    result_turistico1, result_super1, somapontos1, result_total1;
+    int result_pop2, result_area2, result_PIB2, result_des_pop2, result_PIB_percap2,
+    result_turistico2, result_super2, somapontos2, result_total2;
+    unsigned long int populacao1, populacao2;
     float area1, PIB1, area2, PIB2, densidade_pop1, PIB_percap1, densidade_pop2, PIB_percap2;
+    float super1, super2;
 
-    // Cadastro das Cartas:
+    // Cadastro das Cartas: 
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
     // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
     
@@ -70,6 +76,41 @@ int main() {
     densidade_pop2 = populacao2 / area2;
     PIB_percap2 = PIB2 / populacao2;
 
+    // Calcular super poder
+
+    super1 = ((float)populacao1 + (float)turisticos1 + area1 + PIB1 + PIB_percap1) - densidade_pop1;
+    super2 = ((float)populacao2 + (float)turisticos2 + area2 + PIB2 + PIB_percap2) - densidade_pop2;
+
+    // Comparativo Atributos Carta1 vs Carta2
+
+    result_pop1 = populacao1 > populacao2;
+    result_pop2 = populacao2 > populacao1;
+    result_area1 = area1 > area2;
+    result_area2 = area2 > area1;
+    result_PIB1 = PIB1 > PIB2;
+    result_PIB2 = PIB2 > PIB1;
+    result_turistico1 = turisticos1 > turisticos2;
+    result_turistico2 = turisticos2 > turisticos1;
+    result_PIB_percap1 = PIB_percap1 > PIB_percap2;
+    result_PIB_percap2 = PIB_percap2 > PIB_percap1;
+    result_des_pop1 = densidade_pop1 < densidade_pop2;
+    result_des_pop2 = densidade_pop2 < densidade_pop1;
+    result_super1 = super1 > super2;
+    result_super2 = super2 > super1;
+
+    // Somar pontuação das Cartas
+
+    somapontos1 = result_area1 + result_des_pop1 + result_PIB1 + result_PIB_percap1 + 
+                    result_pop1 + result_super1 + result_turistico1;
+
+    somapontos2 = result_area2 + result_des_pop2 + result_PIB2 + result_PIB_percap2 + 
+                    result_pop2 + result_super2 + result_turistico2;
+
+    // Comparar carta vencedora
+
+    result_total1 = somapontos1 > somapontos2;
+    result_total2 = somapontos2 > somapontos1;
+
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
@@ -95,5 +136,17 @@ int main() {
     printf("Densidade Populacional: %.2f hab/km²\n", densidade_pop2);
     printf("PIB per Capita: %.2f reais\n", PIB_percap2);
 
+    // Imprimir resultado das comparações e carta campeã (1) vencedor (0) perdedor
+
+    printf("\n Comparação de Cartas: \n");
+    printf("População: Carta 1 (%d) - Carta 2 (%d)\n", result_pop1, result_pop2);
+    printf("Área: Carta 1 (%d) - Carta 2 (%d)\n", result_area1, result_area2);
+    printf("PIB: Carta 1 (%d) - Carta 2 (%d)\n", result_PIB1, result_PIB2);
+    printf("Pontos Turísticos: Carta 1 (%d) - Carta 2 (%d)\n", result_turistico1, result_turistico2);
+    printf("Densidade Populacional: Carta 1 (%d) - Carta 2 (%d)\n", result_des_pop1, result_des_pop2);
+    printf("PIB Per Capita: Carta 1 (%d) - Carta 2 (%d)\n", result_PIB_percap1, result_PIB_percap2);
+    printf("Super Poder: Carta 1 (%d) - Carta 2 (%d)\n", result_super1, result_super2);
+    printf("Carta Vencedora com mais pontos: Carta 1 (%d) - Carta 2 (%d)\n", result_total1, result_total2);
+  
     return 0;
 }
